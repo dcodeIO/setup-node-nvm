346e64e09bb91a0e59933c428c289c36c23620eb
dcode <dcode@dcode.io>
2019-09-11 13:56:46 +0200

    make it work

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const child_process = require("child_process");
+const path = require("path");
 const core = require("@actions/core");
 const version = core.getInput("node-version");
 const mirror = core.getInput("node-mirror");
-var child = child_process.spawnSync("bash", [ "install.sh", version, mirror ], { stdio: "inherit", encoding: "utf8" });
-if (child.status != 0) throw Error("installation failed with " + child.status);
-var nodePath;
-var npmPath;
-try {
-  nodePath = /SETUP_NODE_NVM_NODE: ([^\n]+)/.exec(child.stdout)[1];
-  npmPath = /SETUP_NODE_NVM_NPM: ([^\n]+)/.exec(child.stdout)[1];
-} catch (e) {
-  throw Error("missing node/npm path in output");
-}
-console.log("Using node: " + nodePath);
-core.setPath(nodePath);
-console.log("Using npm: " + npmPath);
-core.setPath(npmPath);
+const child = child_process.spawn("bash", [ "install.sh", version, mirror ], { cwd: __dirname });
+const stdout = [];
+child.stdout.on("data", out => {
+  stdout.push(out);
+  process.stdout.write(out);
+});
+child.stderr.on("data", out => {
+  process.stderr.write(out);
+});
+child.on("close", code => {
+  if (code !== 0) throw Error("installation failed with " + code);
+  const out = Buffer.concat(stdout).toString("utf8");
+  var nodePath;
+  var npmPath;
+  try {
+    nodePath = /SETUP_NODE_NVM_NODE: ([^\n]+)/.exec(out)[1];
+    npmPath = /SETUP_NODE_NVM_NPM: ([^\n]+)/.exec(out)[1];
+  } catch (e) {
+    throw Error("missing node/npm path in output");
+  }
+  console.log("Using node: " + nodePath);
+  core.addPath(path.dirname(nodePath));
+  console.log("Using npm: " + npmPath);
+  core.addPath(path.dirname(npmPath));
+});
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 export NVM_NODEJS_ORG_MIRROR="$2"
 chmod +x "$NVM_DIR/nvm.sh"
-"$NVM_DIR/nvm.sh" --no-use
+. "$NVM_DIR/nvm.sh" --no-use
 nvm install $1
 nvm use $1
 npm -g install npm

7bcd1c33eb106ce7ac89c6434887721bc18982c9
dcode <dcode@dcode.io>
2020-03-22 00:38:09 +0100

    Automatically use latest nvm

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 export NVM_NODEJS_ORG_MIRROR="$2"
+git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+pushd  "$NVM_DIR"
+git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+popd
 chmod +x "$NVM_DIR/nvm.sh"
 . "$NVM_DIR/nvm.sh" --no-use
-nvm install $1
-nvm use $1
-npm -g install npm@latest
+nvm install "$1"
 echo "SETUP_NODE_NVM_NODE: $(which node)"
 echo "SETUP_NODE_NVM_NPM: $(which npm)"

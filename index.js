071a3f1e9243c15aa2689a200f3fbaf6934fefd8
dcode <dcode@dcode.io>
2020-04-09 16:35:27 +0200

    ok, what if...

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const child_process = require("child_process");
 const path = require("path");
 const core = require("@actions/core");
+const semver = require("semver");
+const nv = require("@pkgjs/nv");
 
-const version = core.getInput("node-version");
-const mirror = core.getInput("node-mirror");
-
-if (process.platform == "win32") {
-  runScript("powershell", ".\\install.ps1");
-} else {
-  runScript("bash", "install.sh");
+async function resolveVersion(version) {
+  let query = version;
+  if (query) {
+    const versions = await nv(version === "node" ? "current" : version);
+    if (versions.length) {
+      versions.sort((a, b) => semver.rcompare(a.version, b.version));
+      return versions[0].version;
+    }
+  }
+  return version;
 }
 
-function runScript(shell, script) {
+(async () => {
+  const version = await resolveVersion(core.getInput("node-version"));
+  const mirror = core.getInput("node-mirror") || "";
+  if (process.platform == "win32") {
+    runScript("powershell", ".\\install.ps1", version, mirror);
+  } else {
+    runScript("bash", "install.sh", version, mirror);
+  }
+})();
+
+function runScript(shell, script, version, mirror) {
   const child = child_process.spawn(shell, [ script, version, mirror ], { cwd: __dirname });
   const stdout = [];
   child.stdout.on("data", out => {

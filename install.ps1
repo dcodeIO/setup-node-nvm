decc36a707c16a22449f6e89123246edf33e6985
dcode <dcode@dcode.io>
2020-04-09 11:25:09 +0200

    perhaps..

--- install.ps1
+++ install.ps1
@@ -2,6 +2,7 @@ Invoke-WebRequest -Uri https://github.com/coreybutler/nvm-windows/releases/downl
 Expand-Archive nvm.zip -DestinationPath .\mynvm
 .\mynvm\nvm node_mirror $args[1]
 .\mynvm\nvm install $args[0]
+.\mynvm\nvm use $args[0]
 echo "SETUP_NODE_NVM_NVM: $((Get-Command .\mynvm\nvm).Path)"
 echo "SETUP_NODE_NVM_NODE: $((Get-Command node).Path)"
 echo "SETUP_NODE_NVM_NPM: $((Get-Command npm).Path)"

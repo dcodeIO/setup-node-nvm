c1315b0ddd420c653976a9a843b76374756bf306
dcode <dcode@dcode.io>
2020-04-09 19:18:06 +0200

    what

--- install.ps1
+++ install.ps1
@@ -1,12 +1,12 @@
 $NVM_HOME="$((Get-Location).Path)\mynvm"
+[System.Environment]::SetEnvironmentVariable("NVM_HOME","$NVM_HOME",[System.EnvironmentVariableTarget]::User)
 Invoke-WebRequest -Uri https://github.com/coreybutler/nvm-windows/releases/download/1.1.7/nvm-noinstall.zip -OutFile nvm.zip
 Expand-Archive nvm.zip -DestinationPath "$NVM_HOME"
 New-Item -Path "$NVM_HOME\settings.txt" -ItemType File 
-setx PATH "$NVM_HOME;$env:path" -m
-nvm root $NVM_HOME
-nvm node_mirror $args[1]
-nvm install $args[0]
-nvm use $args[0]
-echo "SETUP_NODE_NVM_NVM: $((Get-Command nvm).Path)"
+Invoke-Expression "$NVM_HOME\nvm.exe root $NVM_HOME"
+Invoke-Expression "$NVM_HOME\nvm.exe node_mirror $args[1]"
+Invoke-Expression "$NVM_HOME\nvm.exe install $args[0]"
+Invoke-Expression "$NVM_HOME\nvm.exe use $args[0]"
+echo "SETUP_NODE_NVM_NVM: $NVM_HOME\nvm.exe"
 echo "SETUP_NODE_NVM_NODE: $((Get-Command node).Path)"
 echo "SETUP_NODE_NVM_NPM: $((Get-Command npm).Path)"

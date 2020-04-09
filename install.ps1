a62b3aa7bb5f15e5f33a63924a2dd0a99e60793e
dcode <dcode@dcode.io>
2020-04-09 18:57:26 +0200

    fun

--- install.ps1
+++ install.ps1
@@ -2,10 +2,11 @@ $NVM_HOME="$((Get-Location).Path)\mynvm"
 Invoke-WebRequest -Uri https://github.com/coreybutler/nvm-windows/releases/download/1.1.7/nvm-noinstall.zip -OutFile nvm.zip
 Expand-Archive nvm.zip -DestinationPath "$NVM_HOME"
 New-Item -Path "$NVM_HOME\settings.txt" -ItemType File 
-"$NVM_HOME\nvm" root $NVM_HOME
-"$NVM_HOME\nvm" node_mirror $args[1]
-"$NVM_HOME\nvm" install $args[0]
-"$NVM_HOME\nvm" use $args[0]
-echo "SETUP_NODE_NVM_NVM: $((Get-Command $NVM_HOME\nvm).Path)"
+setx PATH "$NVM_HOME;$env:path" -m
+nvm root $NVM_HOME
+nvm node_mirror $args[1]
+nvm install $args[0]
+nvm use $args[0]
+echo "SETUP_NODE_NVM_NVM: $((Get-Command nvm).Path)"
 echo "SETUP_NODE_NVM_NODE: $((Get-Command node).Path)"
 echo "SETUP_NODE_NVM_NPM: $((Get-Command npm).Path)"

95b79d1e43008418044c51ae8aecb89044d2ec80
Daniel Wirtz <dcode@dcode.io>
2020-04-10 19:31:10 +0200

    Add various compatibility layers (#5)

--- index.js
+++ index.js
@@ -1,30 +1,65 @@
 const child_process = require("child_process");
 const path = require("path");
 const core = require("@actions/core");
-const version = core.getInput("node-version");
-const mirror = core.getInput("node-mirror");
-const child = child_process.spawn("bash", [ "install.sh", version, mirror ], { cwd: __dirname });
-const stdout = [];
-child.stdout.on("data", out => {
-  stdout.push(out);
-  process.stdout.write(out);
-});
-child.stderr.on("data", out => {
-  process.stderr.write(out);
-});
-child.on("close", code => {
-  if (code !== 0) throw Error("installation failed with " + code);
-  const out = Buffer.concat(stdout).toString("utf8");
-  var nodePath;
-  var npmPath;
-  try {
-    nodePath = /SETUP_NODE_NVM_NODE: ([^\n]+)/.exec(out)[1];
-    npmPath = /SETUP_NODE_NVM_NPM: ([^\n]+)/.exec(out)[1];
-  } catch (e) {
-    throw Error("missing node/npm path in output");
+const semver = require("semver");
+const nv = require("@pkgjs/nv");
+
+// Utilize @pkgjs/nv to resolve before forwarding to nvm / nvm-windows
+async function resolveVersion(version, mirror) {
+  if (version) {
+    let query = version;
+    switch (query) {
+      case "node": { query = "current"; break; }
+      case "lts/*": { query = "lts_latest"; break; }
+      case "latest": { query = "all"; break; }
+    }
+    const versions = await nv(query, { mirror });
+    if (versions.length) {
+      versions.sort((a, b) => semver.rcompare(a.version, b.version));
+      return versions[0].version;
+    }
   }
-  console.log("Using node: " + nodePath);
-  core.addPath(path.dirname(nodePath));
-  console.log("Using npm: " + npmPath);
-  core.addPath(path.dirname(npmPath));
-});
+  return version;
+}
+
+(async () => {
+  let mirror = core.getInput("node-mirror") || "https://nodejs.org/dist/";
+  let version = await resolveVersion(core.getInput("node-version"), mirror);
+  if (process.platform == "win32") {
+    runScript("powershell", ".\\install.ps1", version, mirror);
+  } else {
+    runScript("bash", "install.sh", version, mirror);
+  }
+})();
+
+function runScript(shell, script, version, mirror) {
+  const child = child_process.spawn(shell, [ script, version, mirror ], { cwd: __dirname });
+  const stdout = [];
+  child.stdout.on("data", out => {
+    stdout.push(out);
+    process.stdout.write(out);
+  });
+  child.stderr.on("data", out => {
+    process.stderr.write(out);
+  });
+  child.on("close", code => {
+    if (code !== 0) throw Error("installation failed with " + code);
+    const out = Buffer.concat(stdout).toString("utf8");
+    var nvmPath;
+    var nodePath;
+    var npmPath;
+    try {
+      nvmPath = /SETUP_NODE_NVM_NVM: ([^\n]+)/.exec(out)[1];
+      nodePath = /SETUP_NODE_NVM_NODE: ([^\n]+)/.exec(out)[1];
+      npmPath = /SETUP_NODE_NVM_NPM: ([^\n]+)/.exec(out)[1];
+    } catch (e) {
+      throw Error("missing nvm/node/npm path in output");
+    }
+    console.log("Using nvm: " + nvmPath);
+    core.addPath(path.dirname(nvmPath));
+    console.log("Using node: " + nodePath);
+    core.addPath(path.dirname(nodePath));
+    console.log("Using npm: " + npmPath);
+    core.addPath(path.dirname(npmPath));
+  });
+}
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="$HOME/mynvm"
 export NVM_NODEJS_ORG_MIRROR="$2"
 git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
 pushd  "$NVM_DIR"
@@ -9,5 +9,6 @@ popd
 chmod +x "$NVM_DIR/nvm.sh"
 . "$NVM_DIR/nvm.sh" --no-use
 nvm install "$1"
+echo "SETUP_NODE_NVM_NVM: $NVM_DIR/nvm.sh"
 echo "SETUP_NODE_NVM_NODE: $(which node)"
 echo "SETUP_NODE_NVM_NPM: $(which npm)"

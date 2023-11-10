99ffdd37aa497a713375e16d88b695f789ef2531
Matthew H. Irby <matt.irby@outlook.com>
2023-11-10 16:01:54 -0500

    Default to nvmrc only if version not specified

--- index.js
+++ index.js
@@ -7,23 +7,25 @@ const { promises: fs } = require("fs");
 
 // Utilize @pkgjs/nv to resolve before forwarding to nvm / nvm-windows
 async function resolveVersion(version, mirror) {
-  let query = version;
-  switch (query) {
-    case "node": { query = "current"; break; }
-    case "lts/*": { query = "lts_latest"; break; }
-    case "latest": { query = "all"; break; }
-    default: {
-      query = await fs.readFile(".nvmrc", "utf8").catch(err => {
-        console.error("Failed to read .nvmrc file", err);
-      });
+  if (version) {
+    let query = version;
+    switch (query) {
+      case "node": { query = "current"; break; }
+      case "lts/*": { query = "lts_latest"; break; }
+      case "latest": { query = "all"; break; }
+    }
+    const versions = await nv(query, { mirror });
+    if (versions.length) {
+      versions.sort((a, b) => semver.rcompare(a.version, b.version));
+      return versions[0].version;
     }
   }
-  const versions = await nv(query, { mirror });
-  if (versions.length) {
-    versions.sort((a, b) => semver.rcompare(a.version, b.version));
-    return versions[0].version;
-  }
-  return version;
+  
+  query = await fs.readFile(".nvmrc", "utf8").catch(err => {
+    console.error("Failed to read .nvmrc file", err);
+  });
+
+  return query;
 }
 
 (async () => {
